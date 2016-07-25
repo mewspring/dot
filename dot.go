f77b4a60ad4a9e00de1348cc0de89a654d821356
mewmew <rnd0x00@gmail.com>
2016-07-25 15:41:31 +0200

    dot: Add skeleton for semantic analysis of DOT files.

--- dot.go
+++ dot.go
@@ -11,7 +11,7 @@ import (
 	"github.com/mewkiz/pkg/errutil"
 )
 
-// ParseFile parses the given Graphviz DOT file.
+// ParseFile parses the given Graphviz DOT file into an AST.
 func ParseFile(path string) (*ast.File, error) {
 	buf, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -20,7 +20,7 @@ func ParseFile(path string) (*ast.File, error) {
 	return ParseBytes(buf)
 }
 
-// Parse parses the given Graphviz DOT file, reading from r.
+// Parse parses the given Graphviz DOT file into an AST, reading from r.
 func Parse(r io.Reader) (*ast.File, error) {
 	buf, err := ioutil.ReadAll(r)
 	if err != nil {
@@ -29,7 +29,7 @@ func Parse(r io.Reader) (*ast.File, error) {
 	return ParseBytes(buf)
 }
 
-// ParseBytes parses the given Graphviz DOT file, reading from b.
+// ParseBytes parses the given Graphviz DOT file into an AST, reading from b.
 func ParseBytes(b []byte) (*ast.File, error) {
 	l := lexer.NewLexer(b)
 	p := parser.NewParser()
@@ -41,10 +41,13 @@ func ParseBytes(b []byte) (*ast.File, error) {
 	if !ok {
 		return nil, errutil.Newf("invalid file type; expected *ast.File, got %T", file)
 	}
+	if err := check(f); err != nil {
+		return nil, errutil.Err(err)
+	}
 	return f, nil
 }
 
-// ParseString parses the given Graphviz DOT file, reading from s.
+// ParseString parses the given Graphviz DOT file into an AST, reading from s.
 func ParseString(s string) (*ast.File, error) {
 	return ParseBytes([]byte(s))
 }
